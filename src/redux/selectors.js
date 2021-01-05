ee6f45cf86114502073199a9313222f78b8c5582
Ivan Kodola <ivan.kodola@mev.com>
2021-01-05 10:43:21 +0200

    added react-redux

---
+++ src/redux/selectors.js
@@ -0,0 +1,29 @@
+import { VISIBILITY_FILTERS } from '../constants';
+import { createSelector } from 'reselect'
+
+const getVisibilityFilter = (state) => state.visibilityFilter;
+
+export const getTodosState = state => state.todos;
+
+export const getTodoList = state =>
+  getTodosState(state) ? getTodosState(state).allIds : [];
+
+export const getTodoById = (state, id) =>
+  getTodosState(state) ? { ...getTodosState(state).byIds[id], id } : {};
+
+export const getTodos = state => getTodoList(state).map(id => getTodoById(state, id));
+
+export const getTodosByVisibilityFilter = createSelector(
+  [ getVisibilityFilter, getTodos ],
+  (visibilityFilter, todos) => {
+    switch (visibilityFilter) {
+      case VISIBILITY_FILTERS.COMPLETED:
+        return todos.filter(todo => todo.completed)
+      case VISIBILITY_FILTERS.INCOMPLETE:
+        return todos.filter(todo => !todo.completed)
+      case VISIBILITY_FILTERS.ALL:
+      default:
+        return todos
+    }
+  }
+);
